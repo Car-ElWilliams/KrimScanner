0e22fec83a917cb88c9c7caffcbe742f1be22fd6
WebDeveloper772 <70943324+WebDeveloper772@users.noreply.github.com>
2020-11-07 22:09:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,15 +28,13 @@ function dates() {
         var d = new Date();
         var n = d.getDate();
         date += n
-        
+
         //WHOLE DATE
         console.log(date)
     }
     
-
 // Today Recent 10 Police Reports
-
-  let promise = fetch ("https://polisen.se/api/events?DateTime=" + date,) 
+  let promise = fetch ("https://polisen.se/api/events?DateTime=") 
   .then(response => {
     console.log(response)
     let otherPromise = response.json()
@@ -44,8 +42,7 @@ function dates() {
   })
   .then(result => {
   topTen(result)
-  
-  function topTen (result){
+    function topTen(result) {
     let ten = []
     for (let x = 0; x < 10; x++) {
       ten.push([result[x]])   
@@ -55,62 +52,46 @@ function dates() {
 //TOP TEN
 city10Loop(ten)
 function city10Loop (ten){
-  for (let index = 0; index < ten.length; index++) {
+  for (let i = 0; i < ten.length; i++) {
     
-    if (ten[index][0].type === 'sammanfattning natt') {
-      splice(ten[index], 1)
+    if (ten[i][0].type === 'sammanfattning natt') {
+      splice(ten[i], 1)
     }
+    console.log(ten[5][0].id , ten.length, i)
     let topTenCities = document.querySelectorAll('#cityTableData')
-    console.log(ten[5][0].id , ten.length, index)
-    topTenCities[index].innerHTML = `<strong>${ten[index][0].location.name}</strong>` 
-    // topTenCities[index].textContent = ten[index][0].location.name
+    topTenCities[i].innerHTML = `<strong>${ten[i][0].location.name}</strong>` 
 
     //TOP TEN SUMMARY
     let summaryTen = document.querySelectorAll('#summary10')
-    summaryTen[index].textContent = ten[index][0].summary
+    summaryTen[i].textContent = ten[i][0].summary
     //TOP TEN DATE
     let dateTen = document.querySelectorAll('#dateTen')
-    // dateTen[index].textContent = ten[index][0].datetime
-       dateTen[index].innerHTML = `<strong>${ten[index][0].datetime}</strong>` 
+    dateTen[i].innerHTML = `<strong>${ten[i][0].datetime}</strong>` 
       }
     }
   }
 })  
 
-
-//Memory
-// let cityInput = document.querySelector('#city').value
-sessionStorage.setItem('City', 'how?')
-localStorage.setItem('City', 'how?')
-
-
+//Submit Button Value fetch
+let submit = document.querySelector('#submit')
+// submit.addEventListener('click', f)
+let city = document.querySelector('#city')
+let crimeType = document.querySelector('select')
 
 // Key Enter to start scanner
-
-// let startScanner = document.queryselctor('#submit')
+let startScanner = document.querySelector('#submit')
 addEventListener('keydown', enter )
 
 function enter (click) {
   if (click.keyCode === 13 && cityInput !== null){
     f()
     console.log(click)
-    
+ }
 }
-}
-
-//Submit Button Value fetch
-let submit = document.querySelector('#submit')
-submit.addEventListener('click', f)
-let city = document.querySelector('#city')
-let crimeType = document.querySelector('select')
 
 //Textarea Results
 let textArea = document.querySelector('[contenteditable="true"]')
-
 function f (){
-  
-  // console.log(city.value, crimeType.value);
-  
   fetch ("https://polisen.se/api/events?locationname=" + city.value + '&type=' + crimeType.value, ) 
   .then(response => {
     console.log(response)
@@ -125,42 +106,24 @@ function f (){
         element.push('<strong>' + [result[index].name + '</strong>' + ' ' + result[index].summary + '' + '<br>' + '<br>'])
       }
       console.log(element);
-    
-      return textArea.innerHTML = `Totala Fall <strong>${element.length} st </strong> <br> <br> ${element.join(' ')} `
+      return textArea.innerHTML = `Totala Fall <strong>${element.length} st </strong> <br> <br> ${element.join(' ')}`
     }
-  } )  
+  })  
 }
 
+//MEMORY SESSION
+if (sessionStorage.getItem("autosave")) {
+  city.value = sessionStorage.getItem("autosave");
+}
+city.addEventListener("change", function() {
+sessionStorage.setItem("autosave", city.value);
+});
 
+//Cookie ex
+document.cookie = 'name=Carel'
 
-
-//Ivestigation button changes theme to dark
-// let onButtonTheme = document.querySelector('#darktheme')
-// let body = document.body
-// onButtonTheme.addEventListener('click', theme)
-// function theme (event){
-    
-//   console.log(event)
-//   if (body.backgroundColor === '#000'){
-//    body.style.backgroundColor = '#fff'
-//    console.log('black');
-//  }
-//   else  if (body.backgroundColor === '#fff'){
-//     body.style.backgroundColor = '#555'
-//     console.log('hello');
-//   }
-  
-// }
-
-
-// GPS https://openstreetmap.org
-
-
-
-
-  // https://polisen.se/kontakt/om-webbplatsen/oppna-data/api-over-polisens-handelser/
-// ?locationname=Göteborg;Mölndal
-// /api/events?type=Misshandel
-// /api/events?DateTime=2018-03-05%2021
-
-//Nearest Police Station https://polisen.se/api/policestations
+//MEMORY LOCAL
+city.value = localStorage.getItem("CityStats");
+city.addEventListener('change', function () {
+  localStorage.setItem('CityStats', city.value)
+})
